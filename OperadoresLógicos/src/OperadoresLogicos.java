f1c3e240fcba89c9902849e0cb69ebad28ce88dd
ClavdiaJFA <cjfa08102013@gmail.com>
2021-07-16 12:06:41 -0500

    Operadores Lógicos

---
+++ OperadoresLógicos/src/OperadoresLogicos.java
@@ -0,0 +1,23 @@
+
+public class OperadoresLogicos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Operadores lógicos
+		 * a == b a es igual a b
+		 * a > b a es mayor que b
+		 * a < b a es menor que
+		 * a != b a es diferente a b
+		 * >= <=
+		 */
+		
+		System.out.println(saludo()); //mando llamar el método
+
+	}
+	
+	public static String saludo() {
+		return "saludo"; //especificar si el método va a devolver un valor
+	}
+
+}
